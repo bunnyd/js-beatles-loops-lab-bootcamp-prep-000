3119edb9c63e33f77c4f7e68890075fbfdfb1a33
Nancy D <nancydo7@gmail.com>
2018-05-22 15:00:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = []
+  
+  for (var i = 0; array < i; i++){
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+
+    
+  }
+  return array
+}
+
+function johnLennoonFacts(){
+  
+}
+function iLoveTheBeatles(n){
+  
+}
